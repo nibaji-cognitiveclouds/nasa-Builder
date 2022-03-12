2a86659a0f3df284ce0d2c3d389a450641db9db2
Nidhun Balaji T R <nidhun.balaji@cognitiveclouds.com>
2022-03-12 23:02:21 +0530

    test home

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -2,13 +2,27 @@
  * @format
  */
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
+import "react-native";
+import React from "react";
+import App from "../App";
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer from "react-test-renderer";
+import Home from "../src/screen/Home";
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+const mockedDispatch = jest.fn();
+
+jest.mock("@react-navigation/native", () => {
+	const actualNav = jest.requireActual("@react-navigation/native");
+	return {
+		...actualNav,
+		useNavigation: () => ({
+			navigate: jest.fn(),
+			dispatch: mockedDispatch,
+		}),
+	};
+});
+
+it("renders <Home/> correctly", () => {
+	renderer.create(<Home />);
 });
