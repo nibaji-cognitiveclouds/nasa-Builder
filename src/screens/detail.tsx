44b117f87de5157c882fd7eb409bd14e0a98fea6
Nidhun Balaji T R <nidhun.balaji@cognitiveclouds.com>
2022-03-24 19:30:30 +0530

    set up details

--- src/screens/detail.tsx
+++ src/screens/detail.tsx
@@ -1,10 +1,44 @@
 /** @format */
 
+import axios from "axios";
 import React from "react";
-import { View } from "react-native";
+import { View, Text, FlatList, ActivityIndicator } from "react-native";
 
-const Details: React.FC = () => {
-	return <View></View>;
+const Details: React.FC = (props: any) => {
+	const [loading, setLoading] = React.useState<boolean>(false);
+	const [error, setError] = React.useState<boolean>(false);
+	const [data, setData] = React.useState<Record<string, any>>({});
+
+	React.useEffect(() => {
+		setLoading(true);
+		axios
+			.get(
+				`https://api.nasa.gov/neo/rest/v1/neo/${props.route.params.id}?api_key=KxdSpbV7eHx1UufmwduVl9KoslktrXGw1D4usxwh`
+			)
+			.then((res) => setData(res.data))
+			.catch((err) => setError(true))
+			.finally(() => {
+				setLoading(false);
+			});
+	}, []);
+	return (
+		<View>
+			{loading ? (
+				<ActivityIndicator />
+			) : error ? (
+				<Text>Error!</Text>
+			) : (
+				<View>
+					<Text>Name : {data.name}</Text>
+					<Text>Nasa Jpl URL : {data.nasa_jpl_url}</Text>
+					<Text>
+						Is Potentially Hazardous :{" "}
+						{data.is_potentially_hazardous_asteroid ? "yes" : "no"}
+					</Text>
+				</View>
+			)}
+		</View>
+	);
 };
 
 export default Details;
